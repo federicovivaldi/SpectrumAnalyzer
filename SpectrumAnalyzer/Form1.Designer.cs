5a50b791573808f876a6d00ed17916059456ddf7
federicovivaldi <32328082+federicovivaldi@users.noreply.github.com>
2017-11-15 17:45:03 +0100

    creating window

--- SpectrumAnalyzer/Form1.Designer.cs
+++ SpectrumAnalyzer/Form1.Designer.cs
@@ -28,12 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.TitleLabel = new System.Windows.Forms.Label();
+            this.FooterLabel = new System.Windows.Forms.Label();
+            this.StartBtn = new System.Windows.Forms.Button();
+            this.StopBtn = new System.Windows.Forms.Button();
+            this.spectrumPlot = new ScottPlot.ScottPlotUC();
+            this.SuspendLayout();
+            // 
+            // TitleLabel
+            // 
+            this.TitleLabel.AutoSize = true;
+            this.TitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TitleLabel.Location = new System.Drawing.Point(349, 9);
+            this.TitleLabel.Name = "TitleLabel";
+            this.TitleLabel.Size = new System.Drawing.Size(243, 31);
+            this.TitleLabel.TabIndex = 0;
+            this.TitleLabel.Text = "Spectrum Analyzer";
+            // 
+            // FooterLabel
+            // 
+            this.FooterLabel.AutoSize = true;
+            this.FooterLabel.Location = new System.Drawing.Point(352, 436);
+            this.FooterLabel.Name = "FooterLabel";
+            this.FooterLabel.Size = new System.Drawing.Size(224, 13);
+            this.FooterLabel.TabIndex = 1;
+            this.FooterLabel.Text = "Topics in Computer Science - Federico Vivaldi";
+            // 
+            // StartBtn
+            // 
+            this.StartBtn.Location = new System.Drawing.Point(666, 12);
+            this.StartBtn.Name = "StartBtn";
+            this.StartBtn.Size = new System.Drawing.Size(75, 23);
+            this.StartBtn.TabIndex = 2;
+            this.StartBtn.Text = "Start";
+            this.StartBtn.UseVisualStyleBackColor = true;
+            this.StartBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // StopBtn
+            // 
+            this.StopBtn.Location = new System.Drawing.Point(756, 12);
+            this.StopBtn.Name = "StopBtn";
+            this.StopBtn.Size = new System.Drawing.Size(75, 23);
+            this.StopBtn.TabIndex = 3;
+            this.StopBtn.Text = "Stop";
+            this.StopBtn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+
+            //
+            //Plot
+            //
+            this.spectrumPlot.Location = new System.Drawing.Point(12, 12);
+            this.spectrumPlot.Name = "scottPlotUC1";
+            this.spectrumPlot.Size = new System.Drawing.Size(1108, 402);
+            this.spectrumPlot.TabIndex = 4;
+
+            //
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(894, 458);
+            this.Controls.Add(this.StopBtn);
+            this.Controls.Add(this.StartBtn);
+            this.Controls.Add(this.FooterLabel);
+            this.Controls.Add(this.TitleLabel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label TitleLabel;
+        private System.Windows.Forms.Label FooterLabel;
+        private System.Windows.Forms.Button StartBtn;
+        private System.Windows.Forms.Button StopBtn;
+        private ScottPlot.ScottPlotUC spectrumPlot;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- SpectrumAnalyzer/Form1.cs
+++ SpectrumAnalyzer/Form1.cs
@@ -7,14 +7,111 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NAudio.Wave; // installed with nuget
+using NAudio.CoreAudioApi;
+using System.Numerics;
 
 namespace SpectrumAnalyzer
 {
     public partial class Form1 : Form
     {
+        public WaveIn wi;
+        public BufferedWaveProvider bwp;
+        public Int32 envelopeMax;
+
+        private int RATE = 44100; // sample rate of the sound card
+        private int BUFFERSIZE = (int)Math.Pow(2, 13);
+
         public Form1()
         {
             InitializeComponent();
+
+            WaveIn wi = new WaveIn();
+            wi.DeviceNumber = 0;
+            wi.WaveFormat = new NAudio.Wave.WaveFormat(RATE, 1);
+
+            wi.DataAvailable += new EventHandler<WaveInEventArgs>(wi_DataAvailable);
+            bwp = new BufferedWaveProvider(wi.WaveFormat);
+            bwp.BufferLength = BUFFERSIZE * 2;
+
+            bwp.DiscardOnBufferOverflow = true;
+            wi.StartRecording();
+        }
+
+        void wi_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            bwp.AddSamples(e.Buffer, 0, e.BytesRecorded);
+        }
+
+        public void UpdateAudioGraph()
+        {
+            // read the bytes from the stream
+            int frameSize = BUFFERSIZE;
+            var frames = new byte[frameSize];
+            bwp.Read(frames, 0, frameSize);
+            if (frames.Length == 0) return;
+            if (frames[frameSize - 2] == 0) return;
+
+            // convert it to int32 manually (and a double for scottplot)
+            int SAMPLE_RESOLUTION = 16;
+            int BYTES_PER_POINT = SAMPLE_RESOLUTION / 8;
+            Int32[] vals = new Int32[frames.Length / BYTES_PER_POINT];
+            double[] Ys = new double[frames.Length / BYTES_PER_POINT];
+            double[] Xs = new double[frames.Length / BYTES_PER_POINT];
+            double[] Ys2 = new double[frames.Length / BYTES_PER_POINT];
+            double[] Xs2 = new double[frames.Length / BYTES_PER_POINT];
+            for (int i = 0; i < vals.Length; i++)
+            {
+                // bit shift the byte buffer into the right variable format
+                byte hByte = frames[i * 2 + 1];
+                byte lByte = frames[i * 2 + 0];
+                vals[i] = (int) (short) ((hByte << 8) | lByte);
+                Xs[i] = i;
+                Ys[i] = vals[i];
+                Xs2[i] = (double) i / Ys.Length * RATE / 1000.0; // units are in kHz
+            }
+
+          
+            //update scottplot (FFT, frequency domain)
+            Ys2 = FFT(Ys);
+            spectrumPlot.Xs = Xs2.Take(Xs2.Length / 2).ToArray();
+            spectrumPlot.Ys = Ys2.Take(Ys2.Length / 2).ToArray();
+
+
+            spectrumPlot.UpdateGraph();
+
+            Application.DoEvents();
+
+            spectrumPlot.Update();
+
+            timer1.Enabled = true;
+        }
+
+        public double[] FFT(double[] data)
+        {
+            double[] fft = new double[data.Length]; // this is where we will store the output (fft)
+            Complex[] fftComplex = new Complex[data.Length]; // the FFT function requires complex format
+            for (int i = 0; i < data.Length; i++)
+            {
+                fftComplex[i] = new Complex(data[i], 0.0); // make it complex format (imaginary = 0)
+            }
+            Accord.Math.FourierTransform.FFT(fftComplex, Accord.Math.FourierTransform.Direction.Forward);
+            for (int i = 0; i < data.Length; i++)
+            {
+                fft[i] = fftComplex[i].Magnitude; // back to double
+                //fft[i] = Math.Log10(fft[i]); // convert to dB
+            }
+            return fft;
+            //todo: this could be much faster by reusing variables
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
